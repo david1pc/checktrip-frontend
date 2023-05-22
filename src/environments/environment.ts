05d6c1d83d03e324a80e19e07dee88ca870d94b0
David <davidalejandrorh@hotmail.com>
2023-05-21 20:34:18 -0500

    Se realiza cambio de credenciales amadeus

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  client_id: 'uvrJJAo1YoBOoj1xHhIzGF4QtAzz9Y4G',
-  client_secret: 'u15RANZGQdvt0xSx',
+  client_id: 'aF0MxMaEbtl8Qos0bQTjJngAgP5lRAk9',
+  client_secret: 'zH6AWRD1f7YEisMx',
   url_api_token: 'https://test.api.amadeus.com/v1/security/oauth2/token',
   url_api_checktrip: 'https://checktrip-production.up.railway.app/api/',
 };
