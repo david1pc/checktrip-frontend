8779e6a710b05cd7e9dbfdab041dc6746d951e5b
David <davidalejandrorh@hotmail.com>
2023-05-23 01:17:46 -0500

    Se modifica importacion de env en vuelosservice

--- src/app/vuelos/services/vuelos.service.ts
+++ src/app/vuelos/services/vuelos.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Busqueda, Viajes } from '../interfaces/vuelos.interface';
 import { Observable } from 'rxjs';
 import { ClienteIdaViajes } from '../interfaces/vuelos-bd.interface';
-import { environment } from 'src/environments/environment';
+import { environment } from '../../../../src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
